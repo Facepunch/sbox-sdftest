c8118c6a43f261f45d57c5f5c0a86b2afa519ed9
James King <james.king@facepunchstudios.com>
2024-07-30 16:36:33 +0100

    Tools to help work with debug dumps

--- code/PolygonDebug.cs
+++ code/PolygonDebug.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 using Sandbox.Polygons;
 using Sandbox.Sdf;
@@ -7,21 +8,34 @@ namespace Sandbox;
 
 public sealed class PolygonDebug : Component
 {
-	[Property, TextArea]
+	[Property, TextArea, Group( "Debug Dump" )]
 	public string Source { get; set; }
 
+	[Button( "Apply" ), Group( "Debug Dump" )]
+	public void Reduce()
+	{
+		var parsed = Json.Deserialize<DebugDump>( Source );
 
-	[Property, Range( 0f, 1f, 0.01f )]
-	public float Distance { get; set; }
+		parsed = parsed.Reduce() with { Exception = null };
+
+		Source = Json.Serialize( parsed );
+	}
+
+	[Property, Group( "Parameters" )]
+	public float Min { get; set; } = 0f;
+
+	[Property, Group( "Parameters" )]
+	public float Max { get; set; } = 1f;
+
+
+	[Property, Range( 0f, 1f, 0.002f ), Group( "Parameters" )]
+	public float Fraction { get; set; }
 
 	protected override void DrawGizmos()
 	{
-		using var polyMeshBuilder = PolygonMeshBuilder.Rent();
-
 		Gizmo.Transform = global::Transform.Zero with { Scale = new Vector3( 1024f, 1024f, 1024f ), Position = new Vector3( -8f * 1024f, -8f * 1024f )};
 
-		Gizmo.Draw.Color = Color.White;
-		polyMeshBuilder.FromDebugDump( Source, Distance );
+		PolygonMeshBuilder.RunDebugDump( Source, Min + (Max - Min) * Fraction );
 
 		//Gizmo.Draw.Color = Color.Blue;
 		//polyMeshBuilder.DrawGizmos( 0f, bevelScale );
