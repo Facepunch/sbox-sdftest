3f006181a409e58759e18ad935486da105916585
James King <james.king@facepunchstudios.com>
2024-08-28 16:35:35 +0200

    LoD test

---
+++ code/SdfCellLoader.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+using Sandbox.Sdf;
+using Sandbox.Worlds;
+
+namespace Sandbox;
+
+public sealed class SdfCellLoader : Component, ICellLoader
+{
+	[Property]
+	public WorldParameters Parameters { get; set; }
+
+	[Property]
+	public string Seed { get; set; }
+
+	void ICellLoader.LoadCell( WorldCell cell )
+	{
+		var level = cell.World.Level;
+		var sdfObj = new GameObject( false )
+		{
+			Parent = cell.GameObject,
+			Transform = { Local = new Transform( 0f, Rotation.Identity, 1 << level ) }
+		};
+
+		var sdfWorld = sdfObj.Components.Create<Sdf3DWorld>();
+		var sdfSize = (int)cell.World.CellSize >> level;
+
+		sdfWorld.IsFinite = true;
+		sdfWorld.Size = new Vector3Int( sdfSize, sdfSize, 2048 >> level );
+		sdfWorld.HasPhysics = level == 0;
+
+		sdfObj.Enabled = true;
+
+		_ = GenerateAsync( cell, sdfWorld );
+	}
+
+	private async Task GenerateAsync( WorldCell cell, Sdf3DWorld sdfWorld )
+	{
+		if ( Parameters is null ) return;
+
+		await Task.WorkerThread();
+
+		var voxelRes = (int)(sdfWorld.Size.x * Parameters.Ground.ChunkResolution / Parameters.Ground.ChunkSize);
+		var res = voxelRes;
+
+		var heightmap = new float[res * res];
+
+		Parameters.SampleHeightmap( Seed.FastHash(), res, cell.World.CellSize, cell.Transform.World, heightmap, cell.World.Level );
+
+		await Task.MainThread();
+		await sdfWorld.AddAsync( new HeightmapSdf3D( heightmap, res, sdfWorld.Size.x ), Parameters.Ground );
+	}
+
+	void ICellLoader.UnloadCell( WorldCell cell )
+	{
+
+	}
+}
---
+++ code/WorldParameters.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Sandbox.Sdf;
+using Sandbox.Utility;
+
+namespace Sandbox;
+
+[GameResource( "World Parameters", "world", "Parameters for procedurally generating worlds.", Icon = "public" )]
+public sealed class WorldParameters : GameResource
+{
+	[Property]
+	public Sdf3DVolume Ground { get; set; }
+
+	[Property]
+	public Curve TerrainBias { get; set; }
+
+	[Property]
+	public Curve PlainsCurve { get; set; }
+
+	[Property]
+	public Curve MountainsCurve { get; set; }
+
+	public void SampleHeightmap( int seed, int res, float size, Transform transform, Span<float> result, int level = 0 )
+	{
+		var terrainField = Noise.SimplexField( new Noise.FractalParameters( seed, Frequency: 1f / 8192f, Octaves: 4 ) );
+		var heightField = Noise.SimplexField( new Noise.FractalParameters( seed, Frequency: 1f / 2048f, Octaves: 8 ) );
+
+		var scale = 1f / (1 << level);
+
+		for ( var y = 0; y < res; ++y )
+		for ( var x = 0; x < res; ++x )
+		{
+			var worldPos = (Vector2)transform.PointToWorld( new Vector3( x, y, 0f ) * size / (res - 1) );
+
+			var terrain = terrainField.Sample( worldPos );
+			var height = heightField.Sample( worldPos );
+
+			terrain = TerrainBias.Evaluate( terrain );
+
+			var plainsHeight = PlainsCurve.Evaluate( height );
+			var mountainsHeight = MountainsCurve.Evaluate( height );
+
+			height = plainsHeight + terrain * (mountainsHeight - plainsHeight);
+
+			result[x + y * res] = height * scale;
+		}
+	}
+}
